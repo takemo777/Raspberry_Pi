5ff6b599b522df5403380c7966f90a06ee567c48
takemo777 <kairi.takemoto888@gmail.com>
2023-11-28 10:53:19 +0900

    点滅問題を解消

--- 実習課題/RaspberryPi400で監視カメラを作る/その3/#2.py
+++ 実習課題/RaspberryPi400で監視カメラを作る/その3/#2.py
@@ -6,27 +6,28 @@ from threading import Thread, Event
 from PIL import Image, ImageTk
 import threading, queue
 
-"""停止ボタンを押したら少し待つこと"""
-
 # カメラの起動を監視するクラス
-class VideoCaptureDaemon (threading.Thread):
-   def __init__ (self, video, result_queue):
-    super ().__init__ ()
-    self.daemon = True
-    self.video = video
-    self.result_queue = result_queue
+class VideoCaptureDaemon(threading.Thread):
+    def __init__(self, video, result_queue):
+        super().__init__()
+        self.daemon = True
+        self.video = video
+        self.result_queue = result_queue
 
-   def run (self):
-    self.result_queue.put (cv2.VideoCapture (self.video))
+    def run(self):
+        # カメラのキャプチャを結果キューに格納
+        self.result_queue.put(cv2.VideoCapture(self.video))
 
 # カメラの起動にタイムアウトを設定する関数
-def get_video_capture (video, timeout=5):
-  res_queue = queue.Queue ()
-  VideoCaptureDaemon (video, res_queue).start ()
-  try:
-    return res_queue.get (block=True, timeout=timeout)
-  except queue.Empty:
-    print ('cv2.VideoCapture: could not grab input ( {}). Timeout occurred after {:.2f}s'.format (video, timeout))
+def get_video_capture(video, timeout=5):
+    res_queue = queue.Queue()
+    # カメラの起動監視スレッドを開始
+    VideoCaptureDaemon(video, res_queue).start()
+    try:
+        # タイムアウト内にキャプチャが取得できれば返す
+        return res_queue.get(block=True, timeout=timeout)
+    except queue.Empty:
+        print('cv2.VideoCapture: could not grab input ({}). Timeout occurred after {:.2f}s'.format(video, timeout))
 
 class VideoRecorder:
     def __init__(self, root, cap, size):
@@ -35,11 +36,11 @@ class VideoRecorder:
         self.size = size
         self.recording = False
         self.stop_event = Event()
+        self.photo = None
 
         # Canvasを作成して、フレームを表示するための変数を初期化
         self.canvas = tk.Canvas(root, width=size[0], height=size[1])
         self.canvas.pack()
-        self.photo = None
 
         # レコードボタンを作成し、コマンドとしてtoggle_recordメソッドを指定
         self.record_button = tk.Button(root, text="録画", command=self.toggle_record)
@@ -76,35 +77,40 @@ class VideoRecorder:
         fps = 20.0
         writer = cv2.VideoWriter(self.get_save_path(), fmt, fps, self.size)
 
-        while not self.stop_event.is_set():
+        def update_frame():
             # カメラからフレームを読み込んでリサイズし、ビデオに書き込む
             _, frame = self.cap.read()
             # フレームが空でないかどうかをチェック
-            if frame is None:
-                continue # 空の場合はスキップ
-            # フレームの型がuint8であることを確認
-            frame = frame.astype(np.uint8)
-            frame = cv2.resize(frame, self.size)
-            writer.write(frame)
+            if frame is not None:
+                # フレームの型がuint8であることを確認
+                frame = frame.astype(np.uint8)
+                frame = cv2.resize(frame, self.size)
+                writer.write(frame)
 
-            # フレームをTkinterのCanvasに表示
-            self.photo = ImageTk.PhotoImage(image=Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)))
-            self.canvas.create_image(0, 0, anchor=tk.NW, image=self.photo)
-            self.root.update_idletasks()  # Tkinterのイベントを処理
+                # フレームをTkinterのCanvasに表示
+                self.photo = ImageTk.PhotoImage(image=Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)))
+                self.canvas.create_image(0, 0, anchor=tk.NW, image=self.photo)
+                self.root.update_idletasks()  # Tkinterのイベントを処理
 
-        # 録画が終了したら、ビデオライターのリソースを解放
-        writer.release()
+                if not self.stop_event.is_set():
+                    # 次のフレーム更新をスケジュール
+                    self.root.after(1, update_frame)
+                else:
+                    # 録画を停止し、リソースを解放
+                    writer.release()
+
+        # 最初のフレーム更新を開始
+        update_frame()
 
     def get_save_path(self):
         # 保存先のファイルパスを取得
-        file_path = filedialog.asksaveasfilename(defaultextension=".mp4",
-                                                   filetypes=[("MP4 files", "*.mp4"), ("All files", "*.*")])
+        file_path = filedialog.asksaveasfilename(defaultextension=".mp4", filetypes=[("MP4 files", "*.mp4"), ("All files", "*.*")])
         return file_path
 
 def main():
     # カメラを起動して、ウィンドウを表示
     # cap = cv2.VideoCapture(0) 
-    cap = get_video_capture(0) # タイムアウトを設定
+    cap = get_video_capture(0)  # タイムアウトを設定
     size = (640, 360)
 
     root = tk.Tk()
---
+++ 実習課題/RaspberryPi400で顔認証装置を作る/その1/#1.py
@@ -0,0 +1,65 @@
+import imutils
+import numpy as np
+import cv2
+import tkinter as tk
+from PIL import Image, ImageTk
+
+"カレントディレクトリ: cd 実習課題\RaspberryPi400で顔認証装置を作る"
+
+# 顔検出ループを停止する関数
+def stop_detection():
+    cap.release()
+    cv2.destroyAllWindows()
+    root.destroy()
+
+# メインウィンドウを作成
+root = tk.Tk()
+root.title("Face Detection")
+
+# ビデオフィードを表示するためのラベルを作成
+label = tk.Label(root)
+label.pack(padx=10, pady=10)
+
+# 「停止」ボタンを作成
+stop_button = tk.Button(root, text="停止", command=stop_detection)
+stop_button.pack(pady=10)
+
+# ビデオキャプチャを開く
+cap = cv2.VideoCapture(0)
+
+# モデルを読み込む
+prototxt = "deploy.prototxt"
+model = "res10_300x300_ssd_iter_140000.caffemodel"
+net = cv2.dnn.readNetFromCaffe(prototxt, model)
+
+# 顔検出ループ
+def detect_faces():
+    ret, frame = cap.read()
+    img = imutils.resize(frame, width=400)
+    (h, w) = img.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(img, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+
+    net.setInput(blob)
+    detections = net.forward()
+
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > 0.5:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+            text = "{:.2f}%".format(confidence * 100)
+            y = startY - 10 if startY - 10 > 10 else startY + 10
+            cv2.rectangle(img, (startX, startY), (endX, endY), (0, 0, 255), 2)
+            cv2.putText(img, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
+    # tkinterで表示するために画像をRGB形式に変換
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img_tk = ImageTk.PhotoImage(Image.fromarray(img_rgb))
+    label.img_tk = img_tk
+    label.config(image=img_tk)
+    label.after(10, detect_faces)  # 10ミリ秒後に関数を呼び出す
+
+# 顔検出ループを開始
+detect_faces()
+
+root.mainloop()
