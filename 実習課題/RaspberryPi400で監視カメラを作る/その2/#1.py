44e4052b4d0e4dc0922ec4317508e47d306a09cb
takemo777 <132028969+takemo777@users.noreply.github.com>
2024-07-19 10:06:28 +0900

    修正

--- 実習課題/RaspberryPi400で監視カメラを作る/その2/#1.py
+++ 実習課題/RaspberryPi400で監視カメラを作る/その2/#1.py
@@ -1,8 +1,6 @@
 # 1.プログラムを起動するとRaspberryPi400 に、カメラ映像を静止画として保存するだけの単純なプログラムを作成する
 import cv2
 
-# 実行出来ないときは(cd RaspberryPi400で監視カメラを作る)
-
 cap = cv2.VideoCapture(0)
 ret, frame = cap.read()
 # パスの指定を正確に
